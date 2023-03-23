8912384f7956acd8784e9fc2876141e33c02cc3b
malcoins <autologon@CSED.LAB>
2023-03-23 09:27:53 -0300

    criação da aula 06

---
+++ aula 6/exemplo15.js
@@ -0,0 +1,23 @@
+class Livro{
+    constructor(pNome, pPreco){
+        this.pNome
+        this.pPreco
+    }
+    
+    get Nome(){return this.nome}
+    set Nome(pNome) {this.nome = pNome}
+
+    get Preco(){return this.preco}
+    set Preco(pPreco){this.preco = pPreco}
+
+    calcularDesconto(){
+        this.preco= this.preco - 10
+    }
+}
+
+var obj_livro1 = new Livro("Use a cabeça Java",200)
+obj_livro1.calcularDesconto
+console.Log("O livro "+ obj_livro1.nome+ "custa" +
+obj_livro1.preco+ "reais")
+
+console.log("Preco: "+obj_livro1.preco)
